a55c73f17e79a750d425221330ad14f2fa02aa52
Clement <clement@Clements-MacBook-Pro.local>
2024-11-11 23:45:30 +0100

    add comments for the service file + add a gitignore file for the node_modules

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -13,6 +13,7 @@ export class OlympicService {
 
   constructor(private http: HttpClient) {}
 
+  // Récupère les données du fichier JSON 
   loadInitialData() {
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => {
@@ -26,22 +27,26 @@ export class OlympicService {
     );
   }
 
+  // Récupère les données du fichier JSON en tant qu'observable
   getOlympics() {
     return this.olympics$.asObservable();
   }
 
+  // Récupère le nombre total de Jeux olympiques
   getNumberOfOlympics() {
     return this.olympics$.pipe(
       map(value => value.map(olympic => olympic.participations.length).reduce((acc, val) => acc += val, 0))
     )
   }
 
+  // Récupère le nombre de pays
   getNumberOfCountries() {
     return this.olympics$.pipe(
       map(value => value.length)
     )
   }
   
+  // Formatte les données attendues pour le PieChart
   formatPieChartData(){
     return this.olympics$.pipe(
       map(value => 
@@ -53,6 +58,7 @@ export class OlympicService {
     ))
   }
 
+  // Récupère les données d'un pays avec son id
   getCountryDataById(id: number) {
     return this.olympics$.pipe(map(value =>
       value.find((country: { id: number; }) => id === country.id)
@@ -63,6 +69,7 @@ export class OlympicService {
       }))
   }
 
+  // Récupère le nombre de participation d'un pays
   getNumberOfParticipations(countryData$: Observable<Olympic | undefined>) {
     return countryData$.pipe(map(value => value?.participations.length),
       catchError((error, caught) => {
@@ -71,6 +78,7 @@ export class OlympicService {
       }))
   }
 
+  // Formatte les données attendues pour le LineChart
   formatLineChartData(countryData$: Observable<Olympic | undefined>) {
     return countryData$.pipe(map(value => (
 
@@ -84,6 +92,7 @@ export class OlympicService {
     )))
   }
 
+  // Récupère le nombre total de médailles pour un pays
   getNumberOfMedals(countryData$: Observable<Olympic | undefined>) {
     return countryData$.pipe(map(value => value?.participations.reduce((acc, value) => acc += value.medalsCount, 0),
       catchError((error, caught) => {
@@ -92,6 +101,7 @@ export class OlympicService {
       })))
   }
 
+  // Récupère le nombre d'athlètes participant pour un pays
   getNumberOfAthletes(countryData$: Observable<Olympic | undefined>) {
     return countryData$.pipe(map(value => value?.participations.reduce((acc, value) => acc += value.athleteCount, 0),
     catchError((error, caught) => {
